12786e7b50c3835300fb91616ba8c52ea72e4a1a
cristian navarro leon <cristiannl.0117@gmail.com>
2022-04-28 07:42:28 -0500

    functional programming

--- Exercise01/theCat.js
+++ Exercise01/theCat.js
@@ -1,20 +1,23 @@
 class Cat {
 
-    #loneliness 
+    #hunger
+    #name
+    #loneliness
+    #tiredness
 
     constructor(name) {
-        this.name = name;
-        this.hunger = 5;
+        this.#name = name;
+        this.#hunger = 5;
         this.#loneliness = 5;
-        this.tiredness = 5;
+        this.#tiredness = 5;
     }
 
-    getHunger() {
-        return 'Hunger ' + this.name + ': ' + this.hunger
+    get hunger() {
+        return 'Hunger ' + this.#name + ': ' + this.#hunger
     }
 
-    getTiredness() {
-        return 'Tiredness ' + this.name + ': ' + this.tiredness
+    get tiredness() {
+        return 'Tiredness ' + this.#name + ': ' + this.#tiredness
     }
 
     static feedCats( ...params ) {
@@ -24,41 +27,41 @@ class Cat {
     }
 
     sleep() {
-        console.log(`Sleeping ... ` + this.name);
-        this.tiredness -= 3;
-        this.hunger += 2 ;
+        console.log(`Sleeping ... ` + this.#name);
+        this.#tiredness -= 3;
+        this.#hunger += 2 ;
     }
 
     eat() {
-        console.log('Eating ... ' + this.name);
-        this.hunger -= 2 ;
+        console.log('Eating ... ' + this.#name);
+        this.#hunger -= 2 ;
     }
 
     hugSomeone() {
-        console.log('Hugging someone ... ' + this.name);
+        console.log('Hugging someone ... ' + this.#name);
         this.#loneliness -= 3;
     }
 
     play() {
-        console.log('Playing with other cats ... ' + this.name);
-        this.hunger += 2;
-        this.tiredness += 2;
+        console.log('Playing with other cats ... ' + this.#name);
+        this.#hunger += 2;
+        this.#tiredness += 2;
         this.#loneliness -= 2;
     }
 
     status() {
         console.log(`
-            ${this.name} Status:
-            Hunger: ${this.hunger}
+            ${this.#name} Status:
+            Hunger: ${this.#hunger}
             Loneliness: ${this.#loneliness}
-            Tiredness: ${this.tiredness}
+            Tiredness: ${this.#tiredness}
         `)
     }
 
     reviewNeeds() {
-        let needs = `${this.name} needs: \n`
-        if(this.hunger >= 8) needs += 'I am hungry, need food'
-        if(this.tiredness >= 8) needs += '\nI am so tired, need to sleep'
+        let needs = `${this.#name} needs: \n`
+        if(this.#hunger >= 8) needs += 'I am hungry, need food'
+        if(this.#tiredness >= 8) needs += '\nI am so tired, need to sleep'
         if(this.#loneliness >= 8) needs += '\nI need to huge someone or to play'
         console.log(needs)
     }
@@ -66,7 +69,7 @@ class Cat {
 
 const zenitsu = new Cat('zenitsu')
 const tomasa = new Cat('tomasa')
-console.log(zenitsu.getHunger());
+console.log(zenitsu.hunger);
 zenitsu.status();
 zenitsu.sleep();
 zenitsu.status();
@@ -81,8 +84,8 @@ tomasa.hugSomeone()
 tomasa.status()
 tomasa.reviewNeeds()
 
-console.log(zenitsu.getHunger());
-console.log(tomasa.getHunger());
+console.log(zenitsu.hunger);
+console.log(tomasa.hunger);
 Cat.feedCats(zenitsu,tomasa)
-console.log(zenitsu.getHunger());
-console.log(tomasa.getHunger());
+console.log(zenitsu.hunger);
+console.log(tomasa.hunger);
