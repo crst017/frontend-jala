8d7ec1c8003ddcfce9de550e93aefd42e4b00977
cristian navarro leon <cristiannl.0117@gmail.com>
2022-04-29 16:26:00 -0500

    async await challenge

--- Exercise02/challenge01.js
+++ Exercise02/challenge01.js
@@ -1,15 +1,30 @@
-const arr = [1,2,4,7,4,2,1]
+// const arr = [1,2,4,7,4,2,1]
 
-function findNotRepeated( array ) {
+// function findNotRepeated( array ) {
     
-    const table = array.reduce( (carry, value) => {
-        carry[value] = ( carry[value] || 0 ) + 1;
-        return carry;
-    }, {} ); 
+//     const table = array.reduce( (carry, value) => {
+//         carry[value] = ( carry[value] || 0 ) + 1;
+//         return carry;
+//     }, {} ); 
 
-    for (const key in table) {  
-        if( table[key] === 1) return `Not repeated item is ${key}`
-    }
-}
+//     for (const key in table) {  
+//         if( table[key] === 1) return `Not repeated item is ${key}`
+//     }
+// }
 
-console.log(findNotRepeated(arr));
+// console.log(findNotRepeated(arr));
+
+
+// function delay(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// delay(2000).then(() => console.log('runs after 2 seconds'));
+
+console.log(1);
+setTimeout(() => console.log(2))
+Promise.resolve().then( () => console.log(3))
+Promise.resolve().then( () => setTimeout( () => console.log(4)))
+Promise.resolve().then( () => console.log(5))
+setTimeout(() => console.log(6))
+console.log(7)
