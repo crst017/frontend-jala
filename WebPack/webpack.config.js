ce7882c3ba814b3d91d4cb531e82d6302ce148ac
cristian navarro leon <cristiannl.0117@gmail.com>
2022-05-25 16:20:55 -0500

    webpack mode flags - first approach

--- WebPack/webpack.config.js
+++ WebPack/webpack.config.js
@@ -1,14 +1,26 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'none',
-    // entry: './src/index.js',
-    entry: {
-        app: './src/index.js'
-    },
+    entry: './src/index.js',
     output: {
-        // filename: 'bundle.js'
-        filename: '[name].js',
-        path: path.join(__dirname, 'build')
+        filename: 'main.js',
+        path: path.join(__dirname, 'dev')
+        // path: path.join(__dirname, 'prod')
     }
 }
+
+
+// const path = require('path');
+
+// module.exports = {
+//     mode: 'none',
+//     // entry: './src/index.js',
+//     entry: {
+//         app: './src/index.js'
+//     },
+//     output: {
+//         // filename: 'bundle.js'
+//         filename: '[name].js',
+//         path: path.join(__dirname, 'build')
+//     }
+// }
