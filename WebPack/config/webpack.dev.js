96fe9d0e81c31e314e41299390d1f4a5f126afc3
cristian navarro leon <cristiannl.0117@gmail.com>
2022-05-27 09:59:00 -0500

    fix loaders config

--- WebPack/config/webpack.dev.js
+++ WebPack/config/webpack.dev.js
@@ -1,12 +1,24 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common');
-// const path = require('path');
+const path = require('path');
+const loader = require('sass-loader');
 
 const devConfig = {
     mode: 'development',
     output: {
         filename: 'main.js',
         path: path.join(__dirname + '/..', 'dev')
+    },
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     }
 };
 
