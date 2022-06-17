52a7a7e2ccef2d76d518e9861305d6626e244bbb
cristian navarro leon <cristiannl.0117@gmail.com>
2022-06-17 11:54:07 -0500

    Add: complete and delete task

--- Angular02/toDoApp/src/app/to-do-list/to-do-list.component.ts
+++ Angular02/toDoApp/src/app/to-do-list/to-do-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { ToDo } from "./to-do.model";
 
 @Component({
   selector: 'app-to-do-list',
@@ -7,18 +8,38 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class ToDoListComponent implements OnInit {
-  nuevaTarea = "";
-  listaTareas: string[] = [];
+  nuevaTarea = '';
+  listaTareas : ToDo[] = [];
   constructor () {
-
   }
+
+  completed: boolean = true;
 
   ngOnInit(): void {
 
   }
 
   crearTarea() {
-    this.listaTareas.push(this.nuevaTarea);
+
+    const añadirTarea: ToDo = {
+      name: this.capitalize(this.nuevaTarea),
+      completed: false
+    }
+
+    this.listaTareas.push(añadirTarea);
     this.nuevaTarea = ''
+  }
+
+  onCompleted(i: number) {
+
+    this.listaTareas[i].completed = !this.listaTareas[i].completed
+  }
+
+  onDelete(i: number) {
+    this.listaTareas.splice(i,1);
+  }
+
+  capitalize(str: string) {
+    return str ? str[0].toUpperCase() + str.slice(1) : '';
   }
 }
