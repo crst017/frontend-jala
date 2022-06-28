9fa1256f06bdb0ecacf8d8a25f7adfe366bae0be
cristian navarro leon <cristiannl.0117@gmail.com>
2022-06-28 00:20:03 -0500

    Refactor folders structure

--- PokedexAngular/pokedexAngular/src/app/app.component.ts
+++ PokedexAngular/pokedexAngular/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { dataPokemons, getPokemonImageUri, pokemonColorMap } from '../utils/utils';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +7,7 @@ import { dataPokemons, getPokemonImageUri, pokemonColorMap } from '../utils/util
 })
 
 export class AppComponent {
-  title = 'pokedexAngular';
-
-  pokemons !: any;
 
   constructor() {
-    this.pokemons = dataPokemons.results;
-    this.pokemons.forEach( ( pokemon: any,index: number) => {
-      const pokemonId = index + 1;
-      pokemon.img = getPokemonImageUri(pokemonId);
-      pokemon.bgColor = pokemonColorMap[pokemonId];
-    });
-    console.log(this.pokemons);
   }
 }
