3ff6e48b5f7683e2047a14e88d432e4afd455c51
cristian navarro leon <cristiannl.0117@gmail.com>
2022-07-08 10:51:45 -0500

    Add basic pokemon profile styles

--- PokedexAngular/pokedexAngular/src/app/pokemons/pokemon-routing.module.ts
+++ PokedexAngular/pokedexAngular/src/app/pokemons/pokemon-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokedexComponent } from './pokedex/pokedex.component';
 import { PokemonProfileComponent } from './pokemon-profile/pokemon-profile.component';
-
+import { PokemonResolver } from './pokemon.resolver'
 const routes: Routes = [
-  { path: 'pokedex', component: PokedexComponent },
+  { path: 'pokedex',
+    component: PokedexComponent,
+    resolve: {
+      pokemons: PokemonResolver
+    }
+  },
   { path: 'pokedex/:id', component: PokemonProfileComponent},
   { path: '', redirectTo: '/pokedex', pathMatch: 'full'}
 ];
